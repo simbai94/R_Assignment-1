888c34d75c51179fe043918ada6dfcfa92bf0462
simbai94 <45337208+simbai94@users.noreply.github.com>
2018-12-24 15:21:49 +0200

    Add files via upload

---
+++ Assignment1-R.R
@@ -0,0 +1,12 @@
+# Question One A) - FALSE
+# Question  One B) - TRUE
+
+#QUESTION Two
+#recycling of elements in a vector occurs when a mathematical operation is being done on two vectors of unequal length.
+#[cont] the elements of the shorter vector are operated multiple times on the longer vector.
+
+#QUESTION THREE
+a = c(1,2,3,4,5,6)
+b = c(1,2)
+c = a+b
+print(c)
